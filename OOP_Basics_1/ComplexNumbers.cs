f797d0cdfffefe03a80833c144b36706605a909b
glabojko <glabojko@gmail.com>
2023-07-30 13:33:51 +0200

    ComplexNumber sorting added

---
+++ OOP_Basics_1/ComplexNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Basics_1
+{
+    internal class ComplexNumbers : IComparable<ComplexNumbers>
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public ComplexNumbers(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        //public int CompareTo(ComplexNumbers? other)
+        //{
+
+        //    // return this.Imaginary.CompareTo(other.Imaginary);
+        //    if (this.Imaginary != other.Imaginary)
+        //        return this.Imaginary.CompareTo(other.Imaginary);
+        //    return this.Real.CompareTo(other.Real);
+        //}
+        public int CompareTo(ComplexNumbers? other)
+        {
+
+            // return this.Imaginary.CompareTo(other.Imaginary);
+            if (this.Imaginary != other.Imaginary)
+                return this.Imaginary.CompareTo(other.Imaginary) * -1;
+            return this.Real.CompareTo(other.Real) * -1;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"Real: {Real}, imaginary {Imaginary}";
+        }
+    }
+}
--- OOP_Basics_1/Program.cs
+++ OOP_Basics_1/Program.cs
@@ -1,26 +1,45 @@
 ﻿
 
+using System.Security.Cryptography.X509Certificates;
+
 namespace OOP_Basics_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var cat = new Cat("Mruczek", 7);
-            var dog = new Dog("Azor", 4);
-            var dog2 = new Dog("Alf", 2);
+            //var cat = new Cat("Mruczek", 7);
+            //var dog = new Dog("Azor", 4);
+            //var dog2 = new Dog("Alf", 2);
 
 
-            var sounds = new List<Animal>();
-            sounds.Add(dog);
-            sounds.Add(cat);
-            sounds.Add(dog2);
-            sounds.Sort(new AnimalAgeComparer());
+            //var sounds = new List<Animal>();
+            //sounds.Add(dog);
+            //sounds.Add(cat);
+            //sounds.Add(dog2);
+            //sounds.Sort(new AnimalAgeComparer());
 
-            foreach (var sound in sounds)
+            //foreach (var sound in sounds)
+            //{
+            //    sound.MakeSound();
+            //} 
+
+            var MyList = new List<ComplexNumbers>() 
             {
-                sound.MakeSound();
-            } 
+                new ComplexNumbers(2, 3.5),
+                new ComplexNumbers(5.4, 8),
+                new ComplexNumbers(-2, 14),
+                new ComplexNumbers(3, -8),
+                new ComplexNumbers(3, -10),
+                new ComplexNumbers(2, -8),
+                new ComplexNumbers(2, -8)
+
+            };
+            MyList.Sort();
+            foreach (var item in MyList)
+            {
+                Console.WriteLine(item.ToString());
+            }
         }
     }
 }
